75cd8411a2421e0470c124a644a59cdde39710b5
Praveen Sahu <praveensahu55555gmail.com>
2025-11-28 13:05:09 +0530

    Updated latest code

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,46 @@
 import express from "express";
+import path from "path";
+
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
+const __dirname = path.resolve()
 
 
 
 
-app.get("/", (req, res) => {
+app.get("/health", (req, res) => {
     res.status(200).json({msg:"succes from backend"});
 });
 
-app.listen(ENV.PORT, () => console.log("server is runing on 3000 port: ", ENV.PORT));
+
+
+app.get("/books", (req, res) => {
+    res.status(200).json({msg:"this is book endpint"});
+});
+
+//make our app ready for deployment
+if(ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
+
+
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => {
+           console.log("Server is running on port: ", ENV.PORT);
+        })
+    } catch (error) {
+        console.error("error starting the server", error)
+    }
+}
+
+startServer();
