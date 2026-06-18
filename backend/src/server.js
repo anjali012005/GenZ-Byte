4762856d7968f6d5a3c66d3bf2e994f37a8fc432
Anjali Daharwal <anjalidaharwal5@gmail.com>
2026-06-18 13:16:23 +0530

    fixing cors err

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,7 @@ const __dirname = path.resolve();
 // middleware
 app.use(express.json());
 // credentials:true meaning?? => server allows a browser to include cookies on request
-app.use(cors({ origin: ["http://localhost:5173", ENV.CLIENT_URL,], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", "https://gen-z-byte-git-main-anjalis-projects-4a29e390.vercel.app", ENV.CLIENT_URL,], credentials: true }));
 app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
